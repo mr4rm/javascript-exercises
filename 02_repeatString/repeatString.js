bceeb169a757733e467bed3c6aefcf20a4087efc
mr4rm <mr.armstrong@protonmail.com>
2022-06-12 23:33:47 -0500

    complete 1, 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
+const repeatString = function(string, number) {
+	let answer = '';
+	if (number < 0) {
+		return ('ERROR');
+	}
+		else
+			for (number; number > 0; number--) {
+			answer += `${string}`;
+		}
+			return (answer);
+	};
 
-};
-
+/* repeatString('hey', (Math.floor(Math.random * 1000))); */
+const number = Math.floor(Math.random() * 1000);
+repeatString('', number);
 // Do not edit below this line
 module.exports = repeatString;
