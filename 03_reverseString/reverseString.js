92ad7ff97854a6d999c256cefbdc0cff9e521a08
mr4rm <mr.armstrong@protonmail.com>
2022-06-13 22:00:06 -0500

    complete L3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+	let rev = '';
+	let count = 1;
+	for (let i = word.length; i > 0; i--) {
+ 		rev += word.slice(-count, i );
+		count++;
+	}
+	return rev;
 };
+
+reverseString('hello');
 
 // Do not edit below this line
 module.exports = reverseString;
