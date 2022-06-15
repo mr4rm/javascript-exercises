aff3ee522b4062df3432ba72a5d853afe18e2c33
mr4rm <mr.armstrong@protonmail.com>
2022-06-14 20:16:48 -0500

    working on 4.1

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,10 @@
 const removeFromArray = function() {
-
+	list = [1, 2, 3, 4];
+	for (let i = 0; i < arguments.length; i++) {
+		list.splice(arguments[i], 1);	
+	}
+	return list;	
 };
-
+removeFromArray(2);
 // Do not edit below this line
 module.exports = removeFromArray;
