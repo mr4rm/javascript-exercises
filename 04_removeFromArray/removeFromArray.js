ccd5c7c7f2f08b8b53727f412f7a896ddc5ef0b1
mr4rm <mr.armstrong@protonmail.com>
2022-06-14 21:27:18 -0500

    add comments to 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,8 +1,12 @@
 const removeFromArray = function() {
+// read in list from function call
 	let list = arguments[0];
 	for (let i = 0; i < arguments.length - 1; i++) {
+// find the index number of the wanted item and assign to a variable
 		let remove = list.indexOf(arguments[i + 1]);
+// check that the wanted item exists in the array
 		if (list.includes(arguments[i + 1])) {
+// remove the wanted item
 			list.splice(remove, 1);
 		}
 	}
