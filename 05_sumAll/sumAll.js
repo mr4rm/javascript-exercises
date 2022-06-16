39b1b1d857830c97583198a89a0072f81175cee4
mr4rm <mr.armstrong@protonmail.com>
2022-06-16 16:52:55 -0500

    complete 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,18 +1,18 @@
-const sumAll = function() {
-	// rearrange array lowest to highest if not already so
-	if (arguments[1] < arguments[0]) {
-		let array = [arguments[1], arguments[0]];
-		}
-		else {
-			let array = [arguments[0], arguments[1]];
-		}	
-	
-	const diff = arguments[1] - arguments[0];
-	let sum = arguments[0];
+const sumAll = function(num1, num2) {
+	if (num1 < 0 || num2 < 0) {
+		return ('ERROR');
+	}	
+	if (typeof num1 != 'number' || typeof num2 != 'number') {
+		return ('ERROR');
+	}
+	let array = [num1, num2];
+	array.sort();
+	const diff = array[1] - array[0];
+	let sum = array[0];
 
-	for (let i = 0; i < diff; i++) { // may not go long enough or too long
+	for (let i = 0; i < diff; i++) { 
 	// fill in missing numbers
-		array.splice(i + 1, 0, (arguments[0] + 1 + i)); 
+		array.splice(i + 1, 0, (array[0] + 1 + i)); 
 	// add it up	
 		sum += array[i] + 1;
 	}
