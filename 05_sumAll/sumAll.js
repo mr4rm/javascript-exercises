1043556ddee492981a52fae27b135c99d6a2ae4c
mr4rm <mr.armstrong@protonmail.com>
2022-06-15 20:34:21 -0500

    begin sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,24 @@
 const sumAll = function() {
+	// rearrange array lowest to highest if not already so
+	if (arguments[1] < arguments[0]) {
+		let array = [arguments[1], arguments[0]];
+		}
+		else {
+			let array = [arguments[0], arguments[1]];
+		}	
+	
+	const diff = arguments[1] - arguments[0];
+	let sum = arguments[0];
 
-};
+	for (let i = 0; i < diff; i++) { // may not go long enough or too long
+	// fill in missing numbers
+		array.splice(i + 1, 0, (arguments[0] + 1 + i)); 
+	// add it up	
+		sum += array[i] + 1;
+	}
 
+	return sum;
+}
+sumAll(1, 4);
 // Do not edit below this line
 module.exports = sumAll;
