62b0f2300e694b217e48fad53ac8b39aa78dba46
mr4rm <mr.armstrong@protonmail.com>
2022-06-16 17:55:15 -0500

    complete leap year

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,14 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+	if (year % 4 == 0) {
+		if (((year % 100) == 0) && ((year % 400) != 0)) {
+			return (false);
+		} 
+		return (true);
+	} else {
+		return (false);
+	}
 };
 
+leapYears(1997);
 // Do not edit below this line
 module.exports = leapYears;
