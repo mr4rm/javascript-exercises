eb989ca62a2f02ab7d0193768661012c5a10b849
mr4rm <mr.armstrong@protonmail.com>
2022-06-16 19:00:35 -0500

    finished through 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(num) {
+	const out = ((num - 32) * (5 / 9)).toFixed(1);
+	return (+out);
 };
 
-const ctof = function() {
-
+const ctof = function(num) {
+	const out = ((num * (9 / 5) + 32)).toFixed(1);
+	return (+out);
 };
 
 // Do not edit below this line
